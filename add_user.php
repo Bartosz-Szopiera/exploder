56f8988c96f3e0867ee66c046e62d4abfe8ec667
Bartosz <bartosz.szopiera@gmail.com>
2017-06-01 11:42:34 +0200

    New registration script. Add $response as an universal response to AJAX. Send back reply in JSON. Store password and 'logged-in' information in SESSION.

--- add_user.php
+++ add_user.php
@@ -3,36 +3,47 @@ require_once('mysqli_connect.php');
 if (!session_id()) {
     session_start();
 }
-if(isset($_POST['user_name']) && isset($_POST['password'])) {
-
+if (isset($_SESSION['logged'])) {
+  $response['success'] = false;
+  $response['msg'] = 'You are already registered.';
+}
+elseif(isset($_POST['user_name']) && isset($_POST['password'])) {
   $user_name = mysqli_real_escape_string($dbc, $_POST['user_name']);
   $password = mysqli_real_escape_string($dbc, $_POST['password']);
-
   $query = "SELECT * FROM users WHERE user_name='$user_name'";
   $result = mysqli_query($dbc, $query);
 
-  if (mysqli_num_rows($result) > 0) {
-    echo 'User already exists';
+  if (!$result) {
+    $response['success'] = false;
+    $response['msg'] = 'Error: ' . mysqli_error($dbc);
   }
-  elseif (!$result) {
-    echo 'Error: ' . mysqli_error($dbc);
+  if (mysqli_num_rows($result) > 0) {
+    $response['success'] = false;
+    $response['msg'] = 'User already exists.';
   }
   else {
     $query = "INSERT INTO users (user_name, password)
               VALUES ('$user_name', '$password')";
-    if ($result = mysqli_query($dbc, $query)) {
-      $count = mysqli_num_rows($result);
-      if ($count == 1) {
-        $_SESSION['user_name'] = $user_name;
-        echo $row['user_name'];
-      }
-      else {
-        echo 'Error: ' . mysqli_error($dbc);
-      }
+    $result = mysqli_query($dbc, $query);
+
+    if (!$result) {
+      $response['success'] = false;
+      $response['msg'] = 'Error: ' . mysqli_error($dbc);
     }
     else {
-      echo 'Error: ' . mysqli_error($dbc);
+      $_SESSION['logged'] = true;
+      $_SESSION['user_name'] = $_POST['user_name'];
+      $_SESSION['password'] = $_POST['password'];
+      $response['success'] = true;
+      $response['msg'] = 'User registered.';
+      $response['userName'] = $_POST['user_name'];
     }
   }
 }
+else {
+  $response['success'] = false;
+  $response['msg'] = 'Please fill all required fields.';
+}
+header('Content-type: application/json');
+echo json_encode($response);
 ?>
