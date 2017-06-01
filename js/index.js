005cd3a3af2167ba3cf6d6df261c2a5b6ff2630a
Bartosz <bartosz.szopiera@gmail.com>
2017-06-01 11:49:37 +0200

    Update index.js. Add functions for presenting downloaded data in gui and loading it as current setup for text explosions.

--- js/index.js
+++ js/index.js
@@ -3,57 +3,71 @@ aggregate();
 // letters currently have always 4 pixels in height
 // So 2 is half on the height of the text
 layout(batch, -textLen/2,-2);
-
-function showTab(string) {
+isLogged();
+// ========================================
+// Show or hide tabs acoording to which one
+// was clicked
+function showTab(labelName) {
+  // Panels
   var panels = document.querySelectorAll('.panels .tab');
+  // Labels
   var labels = document.querySelectorAll('.label');
+  // Labels container
   var labelsSection = document.querySelector('.labels');
   for (var i = 0; i < panels.length; i++) {
-    if (panels[i].classList.contains(string)) {
-      // Toggle state of label that was clicked
-      if (labels[i].classList.contains('active')) {
-        // If clicked label was active, deactivate
-        // all tabs
-        labelsSection.classList.remove('active');
-      }
-      else {
-        labelsSection.classList.add('active');
-      }
+    // Panel that was clicked
+    if (panels[i].classList.contains(labelName)) {
+      // Handle labels container
+        // Panel was active
+        if (labels[i].classList.contains('active')) {
+          // Indicate that no panel is now active (for css)
+          labelsSection.classList.remove('active');
+        }
+        // Panel wasn't active
+        else {
+          // Indicate that some panel is now active
+          labelsSection.classList.add('active');
+        }
+      // Toggle this panel state (panel + it's label)
       panels[i].classList.toggle('hidden');
       labels[i].classList.toggle('active');
     }
+    // Other panels
     else {
       // Hide tabs of labels that were not clicked
       panels[i].classList.add('hidden');
       labels[i].classList.remove('active');
     }
   }
+  adjustDBWindow();
 }
-
-function dbWidnowHeight() {
+// ========================================
+// Adjust height of the database window
+function adjustDBWindow() {
   var main = document.querySelector('main');
-  var panels = document.querySelector('div.panels .tab');
+  var panels = document.querySelector('div.panels');
   var mainHeight = parseInt(getComputedStyle(main).height);
   var height = mainHeight - panels.offsetHeight;
   var databaseWindow = document.querySelector('div.database');
   databaseWindow.style.height = height - 1 + 'px';
 }
-
+// ========================================
+// Show the window with settings database
 function showDatabase() {
   var el = document.querySelector('div.database');
   el.classList.toggle('hidden');
-  dbWidnowHeight();
+  adjustDBWindow();
 }
-
 // ========================================
+// Load settings <input> values to the 'get'
+// object for js access
 function takeInput(target) {
   var property = target.name;
-  console.log(get[property]);
-  get[property] = parseFloat(target.value);
-  console.log(get[property]);
+  setup[property] = parseFloat(target.value);
 }
-
 // ========================================
+// Change form 'action' url appropriately to the
+// submit button clicked
 function userForm(target) {
   var form = document.querySelector('#userForm');
   if (target.name == "login") {
@@ -62,18 +76,103 @@ function userForm(target) {
   else if (target.name == "register") {
     form.action = "javascript:register()";
   }
-  hideUserForm();
 }
-
-function hideUserForm() {
+// ========================================
+// Hide user form if server returned session
+// cookie - user is logged-in
+function toggleUserForm(response) {
+  var user = document.querySelector('.userProfile');
+  var userName = document.querySelector('#userName');
   if (document.cookie.search('PHPSESSID') != -1) {
-    form.style.display = 'none';
+    user.classList.add('logged');
+    userName.innerHTML = response.userName;
+    document.querySelector('#userForm').reset();
+  }
+  else {
+    user.classList.remove('logged');
+    userName.innerHTML = 'Nameless';
   }
 }
+// ========================================
+// Load downloaded settings to the table
+var settings;
+function updateTable(response) {
+  var mainTable = document.querySelector('#mainTable tbody');
+  var userTable = document.querySelector('#userTable tbody');
 
+  if (typeof(response.data) != 'undefined') {
+    settings = response.data; //array
+    // Remove previously inserted rows
+    var clones = mainTable.querySelectorAll('.clone');
+    for (var i = 0; i < clones.length; i++) {
+      mainTable.removeChild(clones[i]);
+    }
+    var row = document.getElementById('row');
+    row.removeAttribute('id');
+    // Insert rows
+    for (var i = 0; i < settings.length; i++) {
+      var clone = row.cloneNode(true);
+      clone.classList.add('clone');
+      mainTable.appendChild(clone);
+      clone.children[0].innerHTML = i;
+      for (var j = 1; j < clone.children.length; j++) {
+        clone.children[j].innerHTML = settings[i][j-1];
+      }
+    }
+    row.id = 'row';
+  }
+  // Remove rows form the user table
+  var userTableClones = userTable.querySelectorAll('.clone');
+  for (var i = 0; i < userTableClones.length; i++) {
+    userTable.removeChild(userTableClones[i]);
+  }
+  var clones = mainTable.querySelectorAll('.clone');
+  var user_name = document.getElementById('userName').innerHTML;
+  // Insert rows to the user table
+  for (var i = 0; i < clones.length; i++) {
+    if (user_name == clones[i].children[1].innerHTML) {
+      var clone = clones[i].cloneNode(true);
+      userTable.appendChild(clone);
+      clone.removeAttribute('onmouseenter');
+      clone.removeAttribute('onmouseleave');
+    }
+  }
+}
+// ========================================
+//
+var setupBuffer = []; //temporary container for settings
 
-function logout() {
-  var cookies = document.cookie;
-  var cookieStart = cookies.search('PHPSESSID');
-  var cookieEnd = cookies.
+function loadFromTable(target) {
+  var settingsForm = document.querySelector('div.settings.tab');
+  var inputs = settingsForm.querySelectorAll('input');
+  if (event.type == 'mouseenter' || event.type == 'click') {
+    // Save settings to the buffer
+    for (var i = 0; i < inputs.length; i++) {
+      setupBuffer[i] = setup[keys[i]];
+    }
+  }
+  if (event.type == 'mouseenter') {
+    // Load settings from table to the form inputs
+    var index = parseInt(target.children[0].innerHTML);
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].value = parseFloat(settings[index][i + 2]);
+    }
+  }
+  if (event.type == 'click') {
+    // Load settings from table to the form inputs,
+    // setup object and buffer
+    var index = parseInt(target.children[0].innerHTML);
+    for (var i = 0; i < inputs.length; i++) {
+      var value = parseFloat(settings[index][i + 2])
+      inputs[i].value = value;
+      setup[keys[i]] = value;
+      setupBuffer[i] = value;
+    }
+  }
+  if (event.type == 'mouseleave') {
+    // load settings from the buffer
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].value = setupBuffer[i];
+    }
+  }
 }
