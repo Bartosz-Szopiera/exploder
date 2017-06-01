5c6327ea0f1382710e31eab99174de6c449622f0
Bartosz <bartosz.szopiera@gmail.com>
2017-06-01 11:47:35 +0200

    Upload settings to the database.

--- post_settings.php
+++ post_settings.php
@@ -1,6 +1,33 @@
 <?php
 require_once('mysqli_connect.php');
+if (!session_id()) {
+  session_start();
+}
+if (!isset($_SESSION['logged'])) {
+  die('Please log-in or create a new account.');
+}
+
 $user_name = $_SESSION['user_name'];
-$query = "INSERT INTO settings (user_name, rand, range, speed) ";
-$result = mysqli_query($dbc, $sql);
+$expl_name = $_POST['expl_name'];
+$scale = $_POST['scale'];
+$rand = $_POST['rand'];
+$range = $_POST['range'];
+$density = $_POST['density'];
+$speed = $_POST['speed'];
+
+$query = "INSERT INTO settings (user_name, expl_name,
+          _scale, _rand, _range, _density, _speed)
+          VALUES ('$user_name', '$expl_name',
+          '$scale', '$rand', '$range', '$density', '$speed')";
+$result = mysqli_query($dbc, $query);
+
+if (!$result) {
+  die("Error: " . mysqli_error($dbc));
+}
+
+$response['success'] = true;
+$response['msg'] = 'Settings uploaded.';
+
+header('Content-type: application/json');
+echo json_encode($response);
 ?>
