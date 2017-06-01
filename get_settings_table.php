ef6198dfae914abfe006773f978de70f31a35e78
Bartosz <bartosz.szopiera@gmail.com>
2017-06-01 11:47:57 +0200

    Download settings from the database

--- get_settings_table.php
+++ get_settings_table.php
@@ -1,14 +1,17 @@
 <?php
 require_once('mysqli_connect.php');
-$user_name = $_SESSION['user_name'];
-$query = "SELECT * FROM settings";
-$result = mysqli_query($dbc, $sql);
 
-if ($result) {
-  $row = mysqli_fetch_assoc($result);
-  while ($row) {
-    $data[] = $row;
-  }
-  echo json_encode($data);
+$query = "SELECT * FROM settings";
+$result = mysqli_query($dbc, $query);
+if (!$result) {
+  die('Error: ' . mysqli_error($dbc));
 }
+$data = mysqli_fetch_all($result, MYSQLI_NUM);
+
+$response['success'] = true;
+$response['msg'] = 'Data downloaded.';
+$response['data'] = $data;
+
+header('Content-type: application/json');
+echo json_encode($response);
 ?>
