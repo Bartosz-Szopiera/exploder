24acbae6943d1535fea35728e151409891699d96
Bartosz <bartosz.szopiera@gmail.com>
2017-06-04 16:23:15 +0200

    Add 'subject' property to the $response.

--- get_settings_table.php
+++ get_settings_table.php
@@ -2,15 +2,16 @@
 require_once('mysqli_connect.php');
 
 $query = "SELECT * FROM settings";
-$result = mysqli_query($dbc, $query);
+$result = mysqli_query($dbc,$query);
 if (!$result) {
   die('Error: ' . mysqli_error($dbc));
 }
 $data = mysqli_fetch_all($result, MYSQLI_NUM);
 
 $response['success'] = true;
-$response['msg'] = 'Data downloaded.';
+$response['msg'] = 'Settings data downloaded.';
 $response['data'] = $data;
+$response['subject'] = 'settings';
 
 header('Content-type: application/json');
 echo json_encode($response);
